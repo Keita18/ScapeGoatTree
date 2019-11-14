f4db2721b2ff8f185bda8a47fc5c5af19fcced8d
User <askeita@yahoo.com>
2019-11-15 02:00:25 +0300

    start implementing ScapeGoatTree

---
+++ Src/ScapeGoatTree.kt
@@ -0,0 +1,20 @@
+class ScapeGoatTree<T : Comparable<T>> : AbstractBinarySTree<T>(){
+
+
+    override val size: Int
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+    /**
+     * Adds the specified element to the set.
+     *
+     * @return `true` if the element has been added, `false` if the element is already contained in the set.
+     */
+    override fun add(element: T): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
